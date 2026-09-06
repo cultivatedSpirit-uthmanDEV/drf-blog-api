3cd146feeabb7f92f10ca4b0ca70a325b452bee1
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-09-06 21:17:19 +0100

    chnages to model
    addition of relational field

--- blogAPI/models.py
+++ blogAPI/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
+class Category(models.Model):
+    name = models.TextField()
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE , null=True , blank=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=120)
     content = models.CharField(max_length=500)
+    followers = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at  =  models.DateTimeField(auto_now=True)
     published =  models.BooleanField(default=True)
@@ -16,22 +19,33 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    
-class Category(models.Model):
-    post = models.ManyToManyField(Post)
-    name = models.TextField()
 
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE )
-    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment" )
+    content = models.CharField(max_length= 500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name= "replies")
+    content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
 
-
+class CommentLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
     
