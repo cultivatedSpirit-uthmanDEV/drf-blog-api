9c31c22abcb3619a73549a73d1b9c817890d6115
cultivatedSpirit-uthmanDEV <uthman.dev@outlook.com>
2026-09-06 21:21:12 +0100

    serialization comment
    serialization comment field and nested relationships

--- blogAPI/serializer.py
+++ blogAPI/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment, Like
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,6 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
                 'user',
+                'category',
                 'title' , 
                 'content' ,
                 'created_at', 
@@ -28,3 +29,36 @@ class PostSerializer(serializers.ModelSerializer):
                 "title cannot not be less than three"
             )
         return value
+
+
+    def title(self, value):
+        if Post.objects.filter(title = value).exists():
+            raise serializers.ValidationError(
+            "title already exist")
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'user',
+            'post',
+            'content',
+            'created_at',
+            'updated_at',
+        ]
+
+
+
+
+
+  
+            
+
+    
+
+   
